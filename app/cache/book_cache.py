85206b21e6be70522bdd1c1ff857c7ced8538953
Hritik Dalvi <hrithikdalavi18@gmail.com>
2026-07-25 15:01:39 -0600

    refactor: Update caching logic to use hash for individual book data

--- app/cache/book_cache.py
+++ app/cache/book_cache.py
@@ -17,14 +17,24 @@ def get_cached_books(author: str | None = None) -> list | None:
     return None
 
 def get_cached_book(book_id: int) -> dict | None:
-    key = f"books:{book_id}"
-    cached = redis_client.get(key)
+    key = f"books:hash:{book_id}"
 
-    if cached:
-        print("CACHE HIT -{key}")
-        return json.loads(cached)
-    print("CACHE MISS")
-    return None
+    data = redis_client.hgetall(key)
+
+    if not data:
+        print(f"CACHE MISS -  {key}")
+        return None
+
+    print(f"CACHE HIT - {key}")
+
+    return {
+        "id": int(data["id"]),
+        "title" : data["title"],
+        "author" : data["author"],
+        "pages" : int(data["pages"]),
+        "owner_id" : int(data["owner_id"])
+    }
+
 
 # --- WRITE to cache ---
 
@@ -33,13 +43,22 @@ def set_cached_books(books_data: list, author: str | None = None) -> None:
     redis_client.setex(key, ALL_BOOKS_TTL, json.dumps(books_data))
 
 def set_cached_book(book_id: int, book_data: dict) -> None:
-    key = f"books:{book_id}"
-    redis_client.setex(key, SINGLE_BOOK_TTL, json.dumps(book_data))
+    key = f"books:hash:{book_id}"
+    redis_client.hset(key, mapping={
+        "id": book_data["id"],
+        "title": book_data["title"],
+        "author": book_data["author"],
+        "pages": book_data["pages"],
+        "owner_id": book_data["owner_id"]
+    })
+    redis_client.expire(key, SINGLE_BOOK_TTL)
 
 # --- INVALIDATE cache ---
 
 def invalidate_book(book_id:int) -> None:
-    redis_client.delete(f"books:{book_id}")
+    deleted = redis_client.delete(f"books:hash:{book_id}")
+    if deleted:
+        print(f"Invalidted hash key - books:hash:{book_id}")
 
     list_keys = redis_client.keys("books:all:*")
     if list_keys:
