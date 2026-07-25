5b74c7164827915e150ad535b0e75c5b64e73f02
Hritik Dalvi <hrithikdalavi18@gmail.com>
2026-07-25 12:13:44 -0600

    added single Redis connection

--- app/cache/redis_client.py
+++ app/cache/redis_client.py
@@ -1,6 +1,8 @@
 import redis
 import os
 from dotenv import load_dotenv
+from redis.backoff import NoBackoff
+from redis.retry import Retry
 
 load_dotenv()
 
@@ -9,5 +11,10 @@ redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
     db=0,
-    decode_responses=True           # This returns str instead of bytes    
+    decode_responses=True,          # This returns str instead of bytes
+    socket_connect_timeout=2,
+    socket_timeout=2,
+    retry=Retry(NoBackoff(), 0),    # fail fast instead of retrying a dead connection
+    retry_on_timeout=False,
+    retry_on_error=[],
     )
