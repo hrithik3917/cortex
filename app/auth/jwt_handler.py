c871fd2883874449473f3939a85180b7aa2834bc
Hritik Dalvi <hrithikdalavi18@gmail.com>
2026-08-21 14:17:34 -0600

    fix: Corrected dotenv loading and improved access token creation logic

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,26 +1,27 @@
 import os
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
-from fastapi import HTTPException, status
 from dotenv import load_dotenv
 
 from app.exceptions.app_exceptions import InvalidTokenException
 
-load_dotenv
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 
-
 def create_access_token(data: dict, expire_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
 
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode.update({"exp":expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    if expire_delta:
+        expire = datetime.now(timezone.utc) + expire_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 def decode_access_token(token: str) -> dict:
@@ -28,4 +29,4 @@ def decode_access_token(token: str) -> dict:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTError:
-        raise InvalidTokenException
+        raise InvalidTokenException()    
