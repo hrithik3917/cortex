35872650ce4b5a6c30f901c645bcd22c4af448bc
Hritik Dalvi <hrithikdalavi18@gmail.com>
2026-09-12 17:18:15 -0600

    feat: Updated Docker configuration and API authentication endpoint

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -11,7 +11,7 @@ from app.exceptions.app_exceptions import (
     UserNotFoundException
     )
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/auth/login")
 
 
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db) ):
